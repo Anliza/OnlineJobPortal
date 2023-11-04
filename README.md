92414176c5847cbc59ba017ca5cced8225d247ec
Annliza <nnanliza@gmail.com>
2023-11-04 15:15:04 +0300

    Implement servlets-genericservlet
    Implemented servlets by extending generic servlets

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 OnlineJobPortal
+
+
+Server details:
+Wildfly 26
+Wildfly 26
+
+Servlet Api
+-Handles HTTP REQUEST
+
+Main packages of servlet api 
+    1. javax.servlet - protocol independent servlet
+    2. javax.servlet.http - http servlet
+
+Ways of implementing servlet
+ -Implement servlet interface
+ -Extend Generic Servlet class
+ -Extend HttpServlet Class
+
+GenericServlet
+HttpServlet
+
+ service();
---
+++ src/main/java/com/ann/servlet/HelloAgain.java
@@ -0,0 +1,21 @@
+package com.ann.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class HelloAgain extends GenericServlet{
+
+     @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException,
+            IOException {
+
+        PrintWriter print = servletResponse.getWriter();
+        print.write("<b>Hello world again!!!</b>");
+    }
+    
+}
